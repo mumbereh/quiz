730875a851fe1364564101f4c59161e568647f9a
mumbereh <siirasonh@gmail.com>
2023-10-20 20:41:41 +0300

    fine

--- index.js
+++ index.js
@@ -42,192 +42,169 @@ document.addEventListener("DOMContentLoaded", function () {
         firstnameInput.style.borderColor = firstNameValue === "" ? "red" : "";
         lastnameInput.style.borderColor = lastNameValue === "" ? "red" : "";
     }
+ 
+
     const questions = [
         {
-            question: "What is the biblical significance of the number '40' in the Bible?",
+            question: "In Genesis 29, Asher was one of the sons of Jacob. What were the unique characteristics or blessings associated with Asher's tribe among the twelve tribes of Israel, and how did they contribute to the nation's history?",
             choices: [
-                "(a) Represents perfection",
-                "(b) Symbolizes testing and trial",
-                "(c) Signifies abundance",
+                "(a) Asher's tribe excelled in military leadership and conquests.",
+                "(b) Asher's territory was known for its fertile lands and abundance, playing a vital role in providing for the nation.",
+                "(c) Asher's tribe was renowned for its wisdom and scholarly pursuits.",
+                "(d) Asher's tribe was primarily nomadic, contributing to Israel's mobility."
             ],
-            answer: "b",
+            answer: "b"
         },
         {
-            question: "Who was the prophet that was taken up to heaven in a chariot of fire?",
+            question: "In the context of Asher's role within the twelve tribes of Israel, what symbolism or significance can be attributed to the 'olive oil' mentioned in the blessing of Asher in Deuteronomy 33:24? How did this element influence the tribe's identity?",
             choices: [
-                "(a) Elijah",
-                "(b) Elisha",
-                "(c) Isaiah",
+                "(a) Olive oil symbolized Asher's peace-loving nature and diplomacy among the tribes.",
+                "(b) Olive oil signified Asher's contributions to Israel's religious ceremonies and anointing of leaders.",
+                "(c) Olive oil represented Asher's strength and dominance in trade and commerce.",
+                "(d) Olive oil had no specific meaning in the context of Asher."
             ],
-            answer: "a",
+            answer: "b"
         },
         {
-            question: "In which book of the Bible do we find the story of the three Hebrews in the fiery furnace?",
+            question: "In Genesis 29, what was Jacob's initial emotional response upon seeing Rachel, and what implications did it have for their relationship?",
             choices: [
-                "(a) Genesis",
-                "(b) Daniel",
-                "(c) Ezekiel",
+                "(a) He felt joy, foreseeing a harmonious marriage.",
+                "(b) He wept, sensing an imminent separation from Rachel.",
+                "(c) He laughed, revealing his excitement.",
+                "(d) He bowed, acknowledging her authority."
             ],
-            answer: "b",
+            answer: "b"
         },
         {
-            question: "What is the name of the Apostle Paul's companion who wrote the Gospel of Mark?",
+            question: "Discuss the intricate terms of the agreement between Jacob and Laban for Jacob to marry Rachel. What complexities arose from this arrangement?",
             choices: [
-                "(a) Luke",
-                "(b) Barnabas",
-                "(c) Timothy",
+                "(a) Jacob agreed to work for seven years, but Laban changed the deal.",
+                "(b) Jacob agreed to a dowry, but Rachel insisted on her own terms.",
+                "(c) Laban offered Rachel, but Jacob demanded to marry Leah first.",
+                "(d) Rachel imposed conditions that Jacob had to fulfill."
             ],
-            answer: "b",
+            answer: "a"
         },
         {
-            question: "Who is known as the 'weeping prophet' in the Old Testament?",
+            question: "Examine the symbolism of the number of years Jacob worked for Rachel in Genesis 29. How does this timeframe connect to biblical themes?",
             choices: [
-                "(a) Isaiah",
-                "(b) Jeremiah",
-                "(c) Ezekiel",
+                "(a) It signifies a divine covenant between Jacob and Laban.",
+                "(b) It reflects Jacob's humility and patience in pursuing Rachel.",
+                "(c) It corresponds to the seven days of creation.",
+                "(d) It represents the lunar calendar used by the Israelites."
             ],
-            answer: "b",
-        },
-        // Revised Questions about Offerings and Tithe
-        {
-            question: "What is the biblical tithe typically calculated as a percentage of one's income?",
-            choices: [
-                "(a) 5%",
-                "(b) 10%",
-                "(c) 15%",
-            ],
-            answer: "b",
+            answer: "b"
         },
         {
-            question: "In the Bible, what did Abel offer to God that pleased Him?",
+            question: "Analyze the naming of Jacob's first two sons in Genesis 29. What prophetic significance can be attributed to their names?",
             choices: [
-                "(a) Grains",
-                "(b) Livestock",
-                "(c) Fruits",
+                "(a) Their names symbolize peace and unity among siblings.",
+                "(b) Their names imply divine intervention in family matters.",
+                "(c) Their names suggest harmony and prosperity in their future lives.",
+                "(d) Their names reflect the tribes they would father."
             ],
-            answer: "b",
+            answer: "b"
         },
         {
-            question: "What is the purpose of a tithe according to biblical teachings?",
+            question: "In Genesis 29, how did Laban's actions symbolize the complexities of human relationships, and what consequences followed?",
             choices: [
-                "(a) To support the church",
-                "(b) To fund social programs",
-                "(c) To support the poor and widows",
+                "(a) Laban demonstrated unwavering loyalty to Jacob's cause.",
+                "(b) Laban exposed the rivalry between Leah and Rachel.",
+                "(c) Laban's intervention led to immediate reconciliation.",
+                "(d) Laban's plan resulted in Jacob's swift departure."
             ],
-            answer: "c",
+            answer: "b"
         },
         {
-            question: "Which book in the Bible mentions the story of the widow's mite?",
+            question: "Discuss the controversial action taken by Rachel in Genesis 29 regarding her father's idols. What does this incident reveal about her character and the consequences it had?",
             choices: [
-                "(a) Genesis",
-                "(b) Exodus",
-                "(c) Mark",
-                "(d) Luke",
+                "(a) Rachel's actions demonstrated her devotion to her faith.",
+                "(b) Rachel sought to protect her family from Laban's superstitions.",
+                "(c) Rachel's actions led to a crisis in her relationship with Jacob.",
+                "(d) Rachel's actions were overlooked, emphasizing unity in the family."
             ],
-            answer: "d",
+            answer: "c"
         },
         {
-            question: "What did Zacchaeus promise to do as a sign of his repentance?",
+            question: "In Exodus 4, what does the transformation of Moses' staff into a snake represent in the context of God's message to Pharaoh?",
             choices: [
-                "(a) Give half of his possessions to the poor",
-                "(b) Pay back four times the amount he cheated from others",
-                "(c) Fast for 40 days",
+                "(a) It symbolizes the overthrow of the Egyptian monarchy.",
+                "(b) It foreshadows the role of snakes in the plagues.",
+                "(c) It represents the power struggle between God and Pharaoh.",
+                "(d) It conveys the idea of reconciliation and partnership."
             ],
-            answer: "b",
-        },
-        // Revised Questions from the Last Books of the Old Testament
-        {
-            question: "Which prophet wrote the book of Malachi?",
-            choices: [
-                "(a) Isaiah",
-                "(b) Jeremiah",
-                "(c) Malachi",
-                "(d) Zechariah",
-            ],
-            answer: "c",
+            answer: "c"
         },
         {
-            question: "What does the name 'Malachi' mean?",
+            question: "Examine the spiritual significance of the first plague in Exodus 4, in which God warned Moses about turning the river into blood. How does this event set the tone for the ensuing plagues?",
             choices: [
-                "(a) Messenger of God",
-                "(b) Faithful servant",
-                "(c) King of Israel",
+                "(a) It signifies the purification of the Nile River.",
+                "(b) It represents God's judgment on Egypt's false gods.",
+                "(c) It emphasizes the abundance of life in the river.",
+                "(d) It underscores the need for humility and repentance."
             ],
-            answer: "a",
+            answer: "b"
         },
         {
-            question: "Which Old Testament book contains the prophecies of the coming of Elijah before the great day of the Lord?",
+            question: "Analyze the implications of Moses' speech impediment as a critical element of his mission. How did this condition influence his interactions with Pharaoh and the Israelites?",
             choices: [
-                "(a) Malachi",
-                "(b) Zechariah",
-                "(c) Joel",
+                "(a) It showcased Moses' human limitations and reliance on Aaron.",
+                "(b) It highlighted Moses' eloquence and persuasive abilities.",
+                "(c) It symbolized God's direct communication with Moses.",
+                "(d) It created tension between Moses and Aaron."
             ],
-            answer: "a",
+            answer: "a"
         },
         {
-            question: "Which Old Testament book emphasizes the importance of returning tithes and offerings to God?",
+            question: "Explore the dynamics of the relationship between Moses and Aaron in Exodus 4. How did their partnership shape their roles in the mission to confront Pharaoh?",
             choices: [
-                "(a) Malachi",
-                "(b) Haggai",
-                "(c) Zechariah",
+                "(a) Aaron served as the primary decision-maker.",
+                "(b) Moses and Aaron had distinct and complementary roles.",
+                "(c) Moses relied solely on Aaron's leadership.",
+                "(d) Aaron was Moses' personal advisor."
             ],
-            answer: "a",
+            answer: "b"
         },
         {
-            question: "In which book of the Old Testament is the story of Esther found?",
+            question: "In Exodus 4, what was the significance of Moses' staff and its role in performing miracles? How did it symbolize God's power?",
             choices: [
-                "(a) Nehemiah",
-                "(b) Job",
-                "(c) Esther",
+                "(a) The staff was a mere symbol with no real power.",
+                "(b) The staff represented Moses' authority over Egypt.",
+                "(c) The staff was a conduit for divine power and transformation.",
+                "(d) The staff was a token of Moses' unique abilities."
             ],
-            answer: "c",
-        },
-        // Revised Questions about the Book of Revelation
-        {
-            question: "Who is traditionally believed to be the author of the Book of Revelation?",
-            choices: [
-                "(a) John the Baptist",
-                "(b) John the Apostle",
-                "(c) James",
-            ],
-            answer: "b",
+            answer: "c"
         },
         {
-            question: "What number is often associated with the Book of Revelation, representing perfection and completeness?",
+            question: "Examine the divine instruction for Moses to cast his staff on the ground in Exodus 4. How did this act align with God's purpose and message to Pharaoh?",
             choices: [
-                "(a) 3",
-                "(b) 7",
-                "(c) 12",
+                "(a) It demonstrated Moses obedience to God's commands.",
+                "(b) It initiated a contest of power between God and Pharaoh.",
+                "(c) It was a symbolic gesture of surrender.",
+                "(d) It was a test of the staff's supernatural abilities."
             ],
-            answer: "b",
+            answer: "b"
         },
         {
-            question: "In the Book of Revelation, what is the final destination of the righteous?",
+            question: "In Exodus 4, what was the primary objective of Moses and Aaron's mission to confront Pharaoh, and how did this challenge Pharaoh's authority?",
             choices: [
-                "(a) Purgatory",
-                "(b) Heaven",
-                "(c) Hell",
+                "(a) Their objective was to secure a peaceful coexistence with Egypt.",
+                "(b) Their mission was to assert the superiority of Israel's gods over Egypt.",
+                "(c) They aimed to negotiate a political alliance with Egypt.",
+                "(d) Their mission was to challenge Pharaoh's divine status."
             ],
-            answer: "b",
+            answer: "d"
         },
         {
-            question: "What is the symbolic number of the beast in the Book of Revelation?",
+            question: "Explain the essence of the message that God instructed Moses and Aaron to convey to Pharaoh in Exodus 4. How did this message serve as a test of Pharaoh's willingness to comply?",
             choices: [
-                "(a) 666",
-                "(b) 777",
-                "(c) 888",
+                "(a) The message was a request for Egypt's support in building the Tabernacle.",
+                "(b) The message demanded Pharaoh's surrender of Egypt to the Israelites.",
+                "(c) It called for negotiations and trade agreements between Egypt and Israel.",
+                "(d) The message urged Pharaoh to grant the Israelites their freedom."
             ],
-            answer: "a",
-        },
-        {
-            question: "In the Book of Revelation, what is the final fate of Satan?",
-            choices: [
-                "(a) Eternal torment",
-                "(b) Redemption",
-                "(c) Annihilation",
-            ],
-            answer: "c",
-        },
+            answer: "d"
+        }
     ];
 
     let currentQuestionIndex = 0; // Index to keep track of the current question
